292dc32cb5fe89d2359fc1589ba61b0898d04fec
Tricia Katz <tkatz@tkatz-mbp13.hitronhub.home>
2020-08-05 06:23:36 -0700

    Add support for Full Stack commerce events

--- Optimizely.js
+++ Optimizely.js
@@ -10,6 +10,10 @@ var mpOptimizelyKit = (function (exports) {
         events: {}
     };
 
+    var optimizelyFsDefinedEvents = {
+        events: {}
+    };
+
     function CommerceHandler(common) {
         this.common = common || {};
     }
@@ -19,74 +23,144 @@ var mpOptimizelyKit = (function (exports) {
             event
         );
         expandedEcommerceEvents.forEach(function(expandedEvent) {
-            var optimizelyEvent = {
-                type: 'event',
-                eventName: event.EventName,
-                tags: {}
-            };
-            optimizelyEvent.tags = expandedEvent.EventAttributes || {};
-            if (
-                event.EventCategory ===
-                    mParticle.CommerceEventType.ProductPurchase ||
-                event.EventCategory === mParticle.CommerceEventType.ProductRefund
-            ) {
-                if (expandedEvent.EventName.indexOf('Total') > -1) {
+
+            if (optimizelyDefinedEvents.events[expandedEvent.EventName]) {
+
+                var optimizelyEvent = {
+                    type: 'event',
+                    eventName: event.EventName,
+                    tags: {}
+                };
+                optimizelyEvent.tags = expandedEvent.EventAttributes || {};
+                if (
+                    event.EventCategory ===
+                        mParticle.CommerceEventType.ProductPurchase ||
+                    event.EventCategory === mParticle.CommerceEventType.ProductRefund
+                ) {
+                    if (expandedEvent.EventName.indexOf('Total') > -1) {
+                        if (
+                            event.CustomFlags &&
+                            event.CustomFlags['Optimizely.EventName']
+                        ) {
+                            optimizelyEvent.eventName =
+                                event.CustomFlags['Optimizely.EventName'];
+                        } else {
+                            optimizelyEvent.eventName = expandedEvent.EventName;
+                        }
+                        // Overall purchase event
+                        if (
+                            expandedEvent.EventAttributes &&
+                            expandedEvent.EventAttributes['Total Amount']
+                        ) {
+                            optimizelyEvent.tags.revenue =
+                                expandedEvent.EventAttributes['Total Amount'] * 100;
+                        }
+                        // other individual product events should not have revenue tags
+                        // which are added via the expandCommerceEvent method above
+                    } else {
+                        optimizelyEvent.eventName = expandedEvent.EventName;
+                        if (optimizelyEvent.tags.revenue) {
+                            delete optimizelyEvent.tags.revenue;
+                        }
+                        if (optimizelyEvent.tags.Revenue) {
+                            delete optimizelyEvent.tags.Revenue;
+                        }
+                    }
+                } else {
+                    optimizelyEvent.eventName = expandedEvent.EventName;
                     if (
                         event.CustomFlags &&
                         event.CustomFlags['Optimizely.EventName']
                     ) {
                         optimizelyEvent.eventName =
                             event.CustomFlags['Optimizely.EventName'];
-                    } else {
-                        optimizelyEvent.eventName = expandedEvent.EventName;
-                    }
-                    // Overall purchase event
-                    if (
-                        expandedEvent.EventAttributes &&
-                        expandedEvent.EventAttributes['Total Amount']
-                    ) {
-                        optimizelyEvent.tags.revenue =
-                            expandedEvent.EventAttributes['Total Amount'] * 100;
-                    }
-                    // other individual product events should not have revenue tags
-                    // which are added via the expandCommerceEvent method above
-                } else {
-                    optimizelyEvent.eventName = expandedEvent.EventName;
-                    if (optimizelyEvent.tags.revenue) {
-                        delete optimizelyEvent.tags.revenue;
-                    }
-                    if (optimizelyEvent.tags.Revenue) {
-                        delete optimizelyEvent.tags.Revenue;
                     }
                 }
-            } else {
-                optimizelyEvent.eventName = expandedEvent.EventName;
-                if (
-                    event.CustomFlags &&
-                    event.CustomFlags['Optimizely.EventName']
-                ) {
-                    optimizelyEvent.eventName =
-                        event.CustomFlags['Optimizely.EventName'];
+
+                // Events that are added to the OptimizelyUI will be available on optimizelyEvents.events
+                // Ignore events not included in the Optimizely UI
+                if (optimizelyDefinedEvents.events[optimizelyEvent.eventName]) {
+                    var eventCopy = {};
+                    for (var key in optimizelyEvent) {
+                        eventCopy[key] = optimizelyEvent[key];
+                    }
+                    window['optimizely'].push(eventCopy);
                 }
             }
 
-            // Events that are added to the OptimizelyUI will be available on optimizelyEvents.events
-            // Ignore events not included in the Optimizely UI
-            if (optimizelyDefinedEvents.events[optimizelyEvent.eventName]) {
-                var eventCopy = {};
-                for (var key in optimizelyEvent) {
-                    eventCopy[key] = optimizelyEvent[key];
+            if (optimizelyFsDefinedEvents.events[expandedEvent.EventName]) {
+                var eventKey = event.EventName,
+                userId,
+                userAttributes = {},
+                eventTags = {};
+
+                userAttributes = expandedEvent.EventAttributes || {};
+
+                if (window.mParticle && window.mParticle.Identity) {
+                    var userIdentities = window.mParticle.Identity.getCurrentUser().getUserIdentities()['userIdentites'];
+        
+                    switch(forwarderSettings.userIdField) {
+                        case 'customerId':
+                            userId = userIdentities["customerId"];
+                            break;
+                        case 'email':
+                            userId = userIdentities["email"];
+                            break;
+                        case 'mpid':
+                            userId = userIdentities["mpid"];
+                            break;
+                        case 'other':
+                            userId = userIdentities["other"];
+                            break;
+                        case 'other2':
+                            userId = userIdentities["other2"];
+                            break;
+                        case 'other3':
+                            userId = userIdentities["other3"];
+                            break;
+                        case 'other4':
+                            userId = userIdentities["other4"];
+                            break;
+                        default:
+                            userId = null;
+                    }
                 }
-                window['optimizely'].push(eventCopy);
+
+                if (
+                    event.EventCategory ===
+                        mParticle.CommerceEventType.ProductPurchase ||
+                    event.EventCategory === mParticle.CommerceEventType.ProductRefund
+                ) {
+                    if (expandedEvent.EventName.indexOf('Total') > -1) {
+                            eventKey = expandedEvent.EventName;
+
+                        // Overall purchase event
+                        if (
+                            expandedEvent.EventAttributes &&
+                            expandedEvent.EventAttributes['Total Amount']
+                        ) {
+                            eventTags.revenue =
+                                expandedEvent.EventAttributes['Total Amount'] * 100;
+                        }
+                        // other individual product events should not have revenue tags
+                        // which are added via the expandCommerceEvent method above
+                    } else {
+                        eventKey = expandedEvent.EventName;
+                        if (eventTags.revenue) {
+                            delete eventTags.revenue;
+                        }
+                    }
+                } else {
+                    eventKey = expandedEvent.EventName;
+                }
+
+                window['optimizelyClientInstance'].track(eventKey, userId, userAttributes, eventTags);
             }
+
         });
     };
 
     var commerceHandler = CommerceHandler;
-
-    var optimizelyFsDefinedEvents = {
-        events: {}
-    };
 
     var helpers = {
         arrayToObject: function(array, keyField) {
