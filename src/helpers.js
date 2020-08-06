4e4a2076311ca6f1752e11704088d569056e9a1a
Tricia Katz <tkatz@tkatz-mbp13.hitronhub.home>
2020-08-06 11:11:45 -0700

    Remove support for IE8 and prior in loadScript function as it's not needed

--- src/helpers.js
+++ src/helpers.js
@@ -9,21 +9,7 @@ var helpers = {
     loadScript: function(src, callback) {
         var script = document.createElement('script');
         script.type = 'text/javascript';
-      
-        // IE
-        if (script.readyState) {
-          script.onreadystatechange = function () {
-            if (script.readyState === 'loaded' || script.readyState === 'complete') {
-              script.onreadystatechange = null;
-              callback();
-            }
-          }
-        }
-        // other browsers
-        else {
-          script.onload = callback;
-        }
-      
+        script.onload = callback;
         script.src = src;
         document.head.appendChild(script);
     },
